7c48320731a566fdd14dc5cdb24af973733212e6
grochaarteaga <guillermo.rocha.arteaga@gmail.com>
2020-04-26 21:02:46 +0200

    js not working

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-function my_sum(){
-    var num_1 = document.getElementById("input_1").value;
-    var num_2 = document.getElementById("input_2").value;
-    var total = Number(num_1) + Number(num_2);
-    document.getElementById("demo").innerHTML = total;
+
+function my_sum() {
+  var num_1 = document.getElementById("input_1").value;
+  var num_2 = document.getElementById("input_2").value;
+  var total = Number(num_1) + Number(num_2);
+  document.getElementById("demo").innerHTML = total;
 }
 
-function my_substraction(){
-    var num_1 = document.getElementById("input_1").value;
-    var num_2 = document.getElementById("input_2").value;
-    var total = Number(num_1) - Number(num_2);
-    document.getElementById("demo").innerHTML = total;
+function my_substraction() {
+  var num_1 = document.getElementById("input_1").value;
+  var num_2 = document.getElementById("input_2").value;
+  var total = Number(num_1) - Number(num_2);
+  document.getElementById("demo").innerHTML = total;
 }
-
-
-
