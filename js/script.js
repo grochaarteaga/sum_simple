56d65faaa6a302d26b747b3708f15907c7fd51ab
grochaarteaga <guillermo.rocha.arteaga@gmail.com>
2020-04-26 18:04:20 +0200

    change style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-function myFunction(){
+function my_sum(){
     var num_1 = document.getElementById("input_1").value;
     var num_2 = document.getElementById("input_2").value;
     var total = Number(num_1) + Number(num_2);
     document.getElementById("demo").innerHTML = total;
 }
+
+function my_substraction(){
+    var num_1 = document.getElementById("input_1").value;
+    var num_2 = document.getElementById("input_2").value;
+    var total = Number(num_1) - Number(num_2);
+    document.getElementById("demo").innerHTML = total;
+}
+
 
 
